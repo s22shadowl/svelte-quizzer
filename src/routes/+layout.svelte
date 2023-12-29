db61e7233846d7f3396cd914b19a1827b22472df
s22shadowl <jjerry409@gmail.com>
2023-12-29 17:23:24 +0800

    add default routing

---
+++ src/routes/+layout.svelte
@@ -0,0 +1,10 @@
+<nav>
+	<a href="/">回首頁</a>
+	<a href="/create">設置題目</a>
+	<a href="/import">匯入題目</a>
+	<a href="/play">進行遊戲</a>
+</nav>
+
+<slot />
+
+<p>Create by <a href="https://github.com/s22shadowl">@s22shadowl</a></p>
